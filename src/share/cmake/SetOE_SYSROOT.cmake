4309ca1596b2dbbcf91e4a9c90367d821b472579
Nicole <nmorin@bowdoin.edu>
2015-04-27 17:15:04 -0400

    Sets the sysroot in atom

--- src/share/cmake/SetOE_SYSROOT.cmake
+++ src/share/cmake/SetOE_SYSROOT.cmake
@@ -1,11 +1,13 @@
-# Sets value of OE_SYSROOT based on the AL_DIR env variable
+# Sets value of OE_SYSROOT based on the AL_DIR env variable in nbites.bash
 
-if($ENV{AL_DIR} MATCHES "2.1.0.19")
-  message(STATUS "AL_DIR set to v5")
+file (STRINGS "/home/nmorin/nbites/util/scripts/nbites.bash" BITES_BASH_SCRIPT NEWLINE_CONSUME)
+
+if(${BITES_BASH_SCRIPT} MATCHES "2.1.0.19")
+  message(STATUS "Setting v5 sysroot")
   set( OE_SYSROOT "${TOOLCHAIN_DIR}/libnaoqi-sysroot/" )
 else()
-  message(STATUS "AL_DIR set to v4")
+  message(STATUS "Setting v4 sysroot")
   set( OE_SYSROOT "${TOOLCHAIN_DIR}/sysroot/" )
-endif($ENV{AL_DIR} MATCHES "2.1.0.19")
+endif()
 
-message(STATUS "Here now")
+message(STATUS "If this is incorrect, change your nbites.bash")
